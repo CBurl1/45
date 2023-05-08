06910cbab12d15b5956e223024c5ad9310e1e3a4
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-05-08 00:47:32 -0400

    account route working

--- client/skirec/src/App.js
+++ client/skirec/src/App.js
@@ -1,8 +1,9 @@
+import './App.css';
+import Account from './Account.js';
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Account from './Account.js';
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,27 +11,28 @@ function App() {
           Welcome to the ski pass recommender
         </p>
         <body>
-          <p><Link to="/account">View your account:</Link></p>
-          <p><button>View your recommendation:</button></p> 
-          <p><button>View passes</button></p>
-          <p><button>View resorts:</button></p>
+          <p> <Link to="/account">View your account:</Link> </p>
+          <p> <button>View your recommendation:</button> </p> 
+          <p> <button>View passes</button> </p>
+          <p><button>View resorts:</button> </p>
         </body>
       </header>
-      <Routes>
-        <Route path="/account" element={<Account />} />
-      </Routes>
     </div>
   );
 }
 
-export default function Router() {
+function App() {
   return (
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
+export default App;
 
 
 
