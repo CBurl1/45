8e25da3330622302210c8a923839ecc0e97b32c8
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-05-18 16:42:43 -0400

    styling on navbar

--- client/skirec/src/Navbar.js
+++ client/skirec/src/Navbar.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
-function Navbar() {
+function CustomNavbar() {
   return (
-    <nav>
-      <ul>
-        <button>
-          <Link to="/">Home</Link>
-        </button>
-      </ul>
-    </nav>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="/">Home</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          {/* Add more Nav.Link components for additional navigation items */}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default CustomNavbar;
